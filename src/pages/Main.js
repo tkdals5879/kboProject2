da9aa3c06b2c2f2ffc0c8c5f10154f6b70b3acb7
tkdals5879 <tkdals58799@gmail.com>
2025-05-29 00:23:31 +0900

    main page develop

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';ㅞ
 import axios from 'axios';
 import TeamRankTable from '../component/TeamRankTable';
 
